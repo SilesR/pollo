aa66dadda0c6721301df2347f36776638c516e82
SilesR <rafasiles@gmail.com>
2014-10-08 18:54:09 +0200

    Menús, Fuentes, Inicio desde menú y moneda

---
+++ Assets/Scripts/gameScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Escape)) {
+						Reiniciar ();
+				}
+	
+	}
+	public void Salir1(){
+		Application.Quit ();
+		}
+	public void Reiniciar(){
+		Application.LoadLevel ("menu");
+		}
+	public void Iniciar(){
+		Application.LoadLevel ("Pollo");
+		}
+}
---
+++ Assets/Scripts/siguienteNivel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class siguienteNivel : MonoBehaviour {
+
+	// Use this for initialization
+		void OnTriggerEnter2D(Collider2D other){
+			Siguiente();
+		}
+
+	public void Siguiente(){
+		Application.LoadLevel ("Pollo2");
+	}
+}
